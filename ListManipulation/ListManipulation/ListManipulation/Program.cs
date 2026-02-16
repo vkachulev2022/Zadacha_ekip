152ef9d04c94961b7e8c4124b694b8eea3616ee3
vkachulev2022 <v.kachulev2022@pmg-pernik.com>
2026-02-16 08:35:07 +0200

    Add files via upload

---
+++ ListManipulation/ListManipulation/ListManipulation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] cmd = Console.ReadLine().Split().ToArray();
+            //cw .......
+           // Console.WriteLine("..............");
+            //........
+           
+
+            switch (cmd[0])
+            {
+                case "add":nums.Add(int.Parse(cmd[1]));
+                    Console.WriteLine(string.Join(" ", nums));
+                    break;
+                case "ins":nums.Insert(int.Parse(cmd[1]), int.Parse(cmd[2]));
+                    Console.WriteLine(string.Join(" ", nums));
+                    break;
+                case "contains":
+                    if (nums.Contains(int.Parse(cmd[1])))
+                    {
+                        Console.WriteLine("Yes");
+                        Console.WriteLine(string.Join(" ", nums));
+                      //  Console.WriteLine("Modify");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                    break;
+                case "delete": nums.Remove(int.Parse(cmd[1]));
+                    Console.WriteLine(string.Join(" ", nums));
+                    break;
+                case "remove": nums.RemoveAt(int.Parse(cmd[1]));
+                    Console.WriteLine(string.Join(" ", nums));
+                    break;
+                case "print":
+                    Console.WriteLine(string.Join(" ", nums));
+                    break;
+                case "countOdds": var countOdds = nums.Count(x => x % 2 == 1);
+                    Console.WriteLine($"CountOdds={countOdds}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
